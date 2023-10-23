bfa5b52853d8cc8204b088838a621fd98341c6d3
Hugo <hugo@notainutilis.fr>
2023-10-24 00:45:09 +0200

    unshit my script

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,7 +6,7 @@
 
 # Cleanup sources:
 ## Special cleanup for imported sources of other formats (AdBlock, hosts, etc.).
-find ./sources/_imported -type f -name "*.txt" -exec sed -ri '/^[^#[:punct:]]/s/^/#/; s/^0.0.0.0[[:space:]]*\.//i' {} \;
+find ./sources/_imported -type f -name "*.txt" -exec sed -ri 's/^[^#[:alnum:]]/#&/; s/^0\.0\.0\.0[[:space:]]*//i' {} \;
 ## Normalizes URLs into domains: lowercases, remove leading spaces, protocol (`x://`) `www.` subdomains, everything after `/`, only one space before `#`. Keeps comments intact.
 find ./sources -type f -name "*.txt" -exec sed -ri 'h; s/[^#]*//1; x; s/#.*//; s/.*/\L&/; s/^[[:space:]]*//i; s/^.*:\/\///i; s/^[.*]*//i; s/^www\.//i; s/\/[^[:space:]]*//i; s/[[:space:]].*$/ /i; G; s/(.*)\n/\1/' {} \;
 ## Remove duplicate domains from each source file (keeps repeated comments and empty lines for organization).
--- sources/_imported/README.md
+++ sources/_imported/README.md
@@ -3,3 +3,4 @@ https://github.com/DandelionSprout/adfilt/blob/master/Sensitive%20lists/TabloidR
 https://github.com/MassMove/AttackVectors/blob/master/LocalJournals/fake-local-journals-list.txt
 https://assets.windscribe.com/custom_blocklists/clickbait.txt
 https://github.com/StevenBlack/hosts/blob/master/alternates/fakenews-only/hosts
+https://github.com/antifa-n/pihole
